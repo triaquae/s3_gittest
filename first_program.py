e64454bc48cba5e8f5186e11ed1a707a282887fe
Alex <alex@126.com>
2017-05-08 12:38:46 +0800

    change on win

--- first_program.py
+++ first_program.py
@@ -3,8 +3,9 @@
 def sayhi(name):
     print('Hello world,',name)
     CHANGE AGAIN
-    changed on linux
+    changed on linux changed on windows 
     change on master 
     print('Second change')
     change after rollback
     lalalal
+    changed on windows
